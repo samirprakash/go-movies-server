83cf46f7da68a2fa3bf0f87e147b130240f85da5
Samir Prakash <Samir.Prakash@gmail.com>
2022-02-26 20:17:43 -0600

    updated allowed methods

--- api/middleware.go
+++ api/middleware.go
@@ -3,8 +3,9 @@ package api
 import "net/http"
 
 func (s *Server) enableCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,PATCH")
 		next.ServeHTTP(w, r)
 	})
 }
