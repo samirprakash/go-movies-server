3c505baed4fff3a51981e0009ea71c9944dbdb84
Samir Prakash <Samir.Prakash@gmail.com>
2022-03-01 20:38:02 -0600

    secure delete movie path

--- api/routes.go
+++ api/routes.go
@@ -28,12 +28,12 @@ func (s *Server) routes() http.Handler {
 	r.HandlerFunc(http.MethodPost, "/v1/signin", s.Signin)
 	r.HandlerFunc(http.MethodGet, "/status", s.getStatus)
 	r.HandlerFunc(http.MethodGet, "/v1/movies/:id", s.getMovie)
-	r.HandlerFunc(http.MethodDelete, "/v1/movies/:id", s.deleteMovie)
 	r.HandlerFunc(http.MethodGet, "/v1/movies", s.getMovies)
 	r.HandlerFunc(http.MethodGet, "/v1/genres", s.getGenres)
 	r.HandlerFunc(http.MethodGet, "/v1/genres/:id/movies", s.getMoviesByGenre)
 
 	r.POST("/v1/admin/movies", s.wrap(secure.ThenFunc(s.manageMovie)))
+	r.DELETE("/v1/movies/:id", s.wrap(secure.ThenFunc(s.deleteMovie)))
 
 	return s.enableCORS(r)
 }
